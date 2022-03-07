f34d5b3879b5d8e5c4878936010505bbf8207bae
anil <onealchhetri@gmail.com>
2022-03-07 22:51:13 +0545

    Implemented Direct Binding

---
+++ DirectBindingExhanges/DirectConsumer/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace DirectConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost",
+                Port = 5672,
+                UserName = "anil",
+                Password = "anil",
+                VirtualHost = "HeaderExchange"
+            };
+
+            var conn = factory.CreateConnection();
+
+            var channel = conn.CreateModel();
+
+            var consumer = new EventingBasicConsumer(channel);
+            channel.BasicConsume("direct.queue", false, consumer);
+            consumer.Received += (model, ea) =>
+            {
+                var message = Encoding.UTF8.GetString(ea.Body.ToArray());
+                System.Console.WriteLine($"message: {message}");
+
+                channel.BasicAck(ea.DeliveryTag, false);
+            };
+
+            System.Console.WriteLine("at the end.");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ DirectBindingExhanges/DirectPublisher/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace DirectPublisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost",
+                Port = 5672,
+                UserName = "anil",
+                Password = "anil",
+                VirtualHost = "HeaderExchange"
+            };
+
+            var conn = factory.CreateConnection();
+            var channel = conn.CreateModel();
+
+            channel.QueueDeclare("direct.queue", true, false, false, null);
+
+            //skipping exchageDeclare cause direct binding doesn't required exchanges.
+            // In this case routing key should be same as queue name to which message is send.
+
+            channel.BasicPublish(string.Empty, "direct.queue", false, null, Encoding.UTF8.GetBytes("message without exchange."));
+
+            System.Console.WriteLine("message published.");
+            Console.ReadLine();
+
+        }
+    }
+}
